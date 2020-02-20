cb0967e8baa9f0e66cc59f62bbf2906be3269abb
christian torio <christiantorio@aol.com>
2020-02-19 23:26:53 -0800

    adding more files for project

---
+++ update.sh
@@ -0,0 +1,6 @@
+aws cloudformation update-stack \
+--stack-name $1 \
+--template-body file://$2 \
+--parameters file://$3 \
+--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+--region=us-west-2

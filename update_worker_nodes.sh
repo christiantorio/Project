4ffd35335f81031cc8f74bd49252ec6919e4eeb7
christian torio <christiantorio@aol.com>
2020-03-24 23:18:31 -0700

    changing worker nodes

--- update_worker_nodes.sh
+++ update_worker_nodes.sh
@@ -1,1 +1,1 @@
-aws cloudformation update-stack --stack-name udacity-capstone-nodes --template-body file://worker_nodes.yml --parameters file://worker_nodes_parameters.json --capabilities CAPABILITY_IAM
+aws cloudformation update-stack --stack-name udacity-capstone-nodes --template-body file://infrastructure/worker_nodes.yml --parameters file://infrastructure/worker_nodes_parameters.json --capabilities CAPABILITY_IAM
